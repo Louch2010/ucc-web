381fee0ac7b9f5956975f3cf6385b123c088d685
Louch2010 <1175889450@qq.com>
2016-10-18 16:57:01 +0800

    初始化

---
+++ src/main/java/com/louch2010/ucc/web/App.java
@@ -0,0 +1,13 @@
+package com.louch2010.ucc.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
